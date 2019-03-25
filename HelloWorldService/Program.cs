73b269b74bf848fcb5674f26a3cca54539a58fca
Cedric Derue <cedric.derue@altran.com>
2019-03-25 10:35:47 +0100

    Initial commit

--- HelloWorldService/Program.cs
+++ HelloWorldService/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace HelloWorldService
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Point d'entrée du processus d'hébergement du service.
+        /// </summary>
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                // Le fichier ServiceManifest.XML définit un ou plusieurs noms de types de service.
+                // L'inscription d'un service entraîne le mappage d'un nom de type de service à un type .NET.
+                // Quand Service Fabric crée une instance de ce type de service,
+                // une instance de la classe est créée dans le processus hôte.
+
+                ServiceRuntime.RegisterServiceAsync("HelloWorldServiceType",
+                    context => new HelloWorldService(context)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(HelloWorldService).Name);
+
+                // Empêche ce processus hôte de se terminer, ce qui permet aux services de continuer à s'exécuter.
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
         }
     }
 }
